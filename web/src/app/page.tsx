827a6e41a4c5f77f2d5e503554779d1ab78b5520
unknown <dhoouglas@hotmail.com>
2023-05-23 15:46:03 -0300

    Displaying github user profile

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,15 +1,19 @@
-
+import { cookies } from 'next/headers';
 
 import { Copyright } from '@/components/Copyright';
 import { EmptyMemories } from '@/components/EmptyMemories';
 import { Hero } from '@/components/Hero';
 import { SingIn } from '@/components/SingIn';
+import { Profile } from '@/components/Profile';
 
 
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
+
+
   return (
       <main className="grid grid-cols-2 min-h-screen">
-        
+
         {/* Left */}
         <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)]">
 
@@ -23,7 +27,7 @@ export default function Home() {
           <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes" />
 
           {/* Sing-in */}
-          <SingIn />
+          {isAuthenticated ? <Profile /> : <SingIn />}
 
           {/* Hero */}
           <Hero />
