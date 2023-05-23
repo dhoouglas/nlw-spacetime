5aaa1ec1e7a0bf2e4371177e413792adc63a6dc6
unknown <dhoouglas@hotmail.com>
2023-05-23 16:28:14 -0300

    Getting code from github on mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -13,8 +13,16 @@ import blurBg from './src/assets/bg-blur.png';
 import Stripes from './src/assets/stripes.svg';
 import NLWLogo from './src/assets/nlw-spacetime-logo.svg';
 import { styled } from 'nativewind';
-
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import { useEffect } from 'react';
+ 
 const StyledStripes = styled(Stripes);
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint: 'https://github.com/settings/connections/applications/42f40c8912286a540b35',
+};
 
 export default function App() {
 
@@ -23,6 +31,31 @@ export default function App() {
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   });
+
+  const [request, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: '42f40c8912286a540b35',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  );
+
+  useEffect(() => {
+    /*console.log(makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),)*/
+      
+    console.log(response);
+
+    if (response?.type === 'success') {
+      const { code } = response.params;
+
+      console.log(code);
+    }
+  }, [response]);
 
   if (!hasLoadedFonts) {
     return null;
@@ -51,6 +84,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signInWithGithub()}
         >
           <Text className="font-ald text-sm uppercase text-black">
             Cadastrar Lembrança
