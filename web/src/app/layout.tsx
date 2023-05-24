9680f6333d9a3eb9141db4101e3dfb7b5df808bd
unknown <dhoouglas@hotmail.com>
2023-05-24 12:27:33 -0300

    Defined route layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,13 @@
 import { ReactNode } from 'react'
 import './globals.css'
 import { Roboto_Flex as Roboto, Bai_Jamjuree as BaiJamjuree } from 'next/font/google'
+import { EmptyMemories } from '@/components/EmptyMemories'
+import { Hero } from '@/components/Hero'
+import { Profile } from '@/components/Profile'
+import { SingIn } from '@/components/SingIn'
+import { Copyright } from '@/components/Copyright'
+import { cookies } from 'next/headers'
+
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 const baiJamjuree = BaiJamjuree({ subsets: ['latin'], weight: '700', variable: '--font-bai-jamjuree' })
@@ -15,10 +22,44 @@ export default function RootLayout({
 }: {
   children: ReactNode
 }) {
+
+  const isAuthenticated = cookies().has('token')
+
   return (
     <html lang="en">
       <body className={`${roboto.variable} ${baiJamjuree.variable} font-sans bg-gray-900 text-gray-100`}>
-        {children}
+        
+
+        <main className="grid grid-cols-2 min-h-screen">
+
+          {/* Left */}
+          <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)]">
+
+            {/* Blur */}
+            <div className={`
+            absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 bg-purple-700 
+            opacity-50 rounded-full blur-full
+          `} />
+
+            {/* Stripes */}
+            <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes" />
+
+            {/* Sing-in */}
+            {isAuthenticated ? <Profile /> : <SingIn />}
+
+            {/* Hero */}
+            <Hero />
+
+            {/* Copyright */}
+            <Copyright />
+          </div>
+
+          {/* Right */}
+          <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)]">
+            {children}
+          </div>
+
+        </main>
       </body>
     </html>
   )
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,46 +1,10 @@
 import { cookies } from 'next/headers';
 
-import { Copyright } from '@/components/Copyright';
 import { EmptyMemories } from '@/components/EmptyMemories';
-import { Hero } from '@/components/Hero';
-import { SingIn } from '@/components/SingIn';
-import { Profile } from '@/components/Profile';
-
 
 export default function Home() {
-  const isAuthenticated = cookies().has('token')
-
 
   return (
-      <main className="grid grid-cols-2 min-h-screen">
-
-        {/* Left */}
-        <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)]">
-
-          {/* Blur */}
-          <div className={`
-            absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 bg-purple-700 
-            opacity-50 rounded-full blur-full
-          `} />
-
-          {/* Stripes */}
-          <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes" />
-
-          {/* Sing-in */}
-          {isAuthenticated ? <Profile /> : <SingIn />}
-
-          {/* Hero */}
-          <Hero />
-
-          {/* Copyright */}
-          <Copyright />
-        </div>
-
-        {/* Right */}
-        <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)]">
-          <EmptyMemories />    
-        </div>
-
-      </main>
+    <EmptyMemories /> 
   )
 }
